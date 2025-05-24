27efbd75f7c52b043cf81a72d533d097d4f9d16d
Milind Mishra <milind.mishra4@gmail.com>
2025-05-24 23:05:04 +0530

    feat: minor fixes

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -4,6 +4,7 @@ import { Skills } from "./sections/skills";
 import { Work } from "./sections/work";
 import { Education } from "./sections/education";
 import type { TResumeSchema } from "../utils/resume.schema";
+import { cn } from "./lib/utils";
 
 interface ResumeProps {
   resume: TResumeSchema;
@@ -12,10 +13,7 @@ interface ResumeProps {
 
 export const Resume: React.FC<ResumeProps> = ({ resume, className = "" }) => {
   return (
-    <div
-      id="resume"
-      className={`max-w-4xl mx-auto bg-white p-8 font-sans ${className}`}
-    >
+    <div id="resume" className={cn("max-w-4xl mx-auto p-8", className)}>
       <Basics basics={resume.basics} />
       <Skills skills={resume.skills} />
       <Work work={resume.work} />
